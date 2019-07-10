9b91d3d60d2c5a6d7558cbf8b4d230c5070c13ee
Corina Allen <StarPerfect@gmail.com>
2019-07-10 08:51:45 -0600

    Complete Iteration 3

---
+++ lib/world_cup.rb
@@ -0,0 +1,18 @@
+class WorldCup
+  attr_reader :year, :teams
+
+  def initialize(year, teams)
+    @year = year
+    @teams = teams
+  end
+
+  def active_players_by_position(position)
+    active_players = []
+    @teams.each do |team|
+      unless team.eliminated?
+        active_players << team.players_by_position(position)
+      end
+    end
+    active_players.flatten
+  end
+end
